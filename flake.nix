467deaf0f1c61e446cf31e83d928c336e65c4e8f
dustinm <dustinmmiao@gmail.com>
2026-03-16 11:05:07 -0400

    manage dependencies with uv and nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "03-232 devenv";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs = { self, nixpkgs }: 
+  let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        python312
+        texliveFull
+        texlivePackages.enumitem
+        uv
+      ];
+    };
+  };
+}
